d70c6ad9a5848aad6cbc3ad66d4f6ba185177ce2
Natalia Venditto <nataliafrontend@gmail.com>
2023-06-01 17:55:37 +0200

    initial commit

---
+++ .arc
@@ -0,0 +1,8 @@
+@app
+cityjsconf
+
+@static
+prune true
+
+@plugins
+enhance/arc-plugin-enhance
---
+++ app/elements/my-link.mjs
@@ -0,0 +1,5 @@
+export default function MyLink({ html }) {
+  return html`
+    <a title="go to sara new page" class="font-mono font-light inline-flex align-items-center pi-3 underline" href="/sara">Go to Sara</a>
+  `
+}
